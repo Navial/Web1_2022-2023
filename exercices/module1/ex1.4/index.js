1de84966fdd53f1fdbca518b5d3831d462ec4c67
Victor Denis <victor.denis@student.vinci.com>
2022-10-12 22:33:30 +0200

    Ex1.4

---
+++ exercices/module1/ex1.4/index.js
@@ -0,0 +1,27 @@
+let etat = 1;
+let timeoutId;
+const red = document.querySelector("#red");
+const orange = document.querySelector("#orange");
+const green = document.querySelector("#green");
+
+timeoutId = setInterval(changeState, 2000);
+
+
+function changeState(){
+    if(etat == 1){
+        red.style.backgroundColor = "red";
+        green.style.backgroundColor = "white";
+        etat ++;
+    }else if (etat == 2){
+        console.log(etat);
+        red.style.backgroundColor = "white";
+        orange.style.backgroundColor = "orange";
+        etat ++;
+    }else{
+        console.log(etat);
+        orange.style.backgroundColor = "white";
+        green.style.backgroundColor = "green";
+        etat = 0;
+    }
+
+}
