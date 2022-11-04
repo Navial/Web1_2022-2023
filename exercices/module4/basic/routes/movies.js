0387c9a113562a883112617e2176e95258642eb2
victor.denis <victor.denis@student.vinci.be>
2022-11-04 19:47:05 +0100

    ex4.1 p2

--- exercices/module4/basic/routes/movies.js
+++ exercices/module4/basic/routes/movies.js
@@ -27,15 +27,50 @@ const MOVIES = [
 
 /* GET all movies. */
 router.get('/', function (req, res, next) {
-  console.log(req.query.minimumduration);
   let filteredList;
-  if (req.query.minimumduration !== null){
+  if (req.query.minimumduration > 0){
     filteredList = MOVIES.filter(
       m => m.duration >= req.query.minimumduration
     );
     }
   console.log("GET /movies");
   res.json(filteredList ?? MOVIES);
+});
+
+/* GET all movies. */
+router.get('/:id', function (req, res) {
+  const movieIndex = MOVIES.findIndex(m => m.id == req.params.id);
+  
+  if(movieIndex <0) return res.status(400);
+  console.log("GET /movies/:id");
+  res.json(MOVIES[movieIndex]);
+});
+
+router.post('/', function (req, res) {
+  const title = req?.body?.title?.lenght !== 0 ? req.body.title : undefined;
+  //const duration = req?.body?.duration?.value > 0 ? req.body.duration : undefined;
+  //const budget = req?.body?.budget?.value > 0 ? req.body.budget : undefined;
+  const duration =req.body.duration;
+  const budget = req.body.budget;
+  const link = req?.body?.link?.lenght !== 0 ? req.body.link : undefined;
+
+  if(!title || !duration || !budget || !link) return res.sendStatus(404);
+  
+  const newId = MOVIES.length +2;
+
+  const newMovie =
+  {
+    id: newId,
+    title: title,
+    duration: duration,
+    budget: budget,
+    link: link
+  }
+  
+  MOVIES.push(newMovie);
+
+  res.json(newMovie);
+
 });
 
 module.exports = router;
